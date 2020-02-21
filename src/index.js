4c2690661cacd842efc57ff6837bbf29f78691a3
doher <alexandr.pletezhov@gmail.com>
2020-02-21 11:06:05 +0300

    feat: add solution to reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.sqrt(Math.pow(n, 2));
+
+    let result = +(n + '').split('').reverse().join('');
+
+    return result;
 }
